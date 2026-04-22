75f2ad4982206e191fa3446bd8719b92b3959380
RitiRai <ritirai0612@gmail.com>
2026-04-22 23:47:36 +0530

    Fix: Switch to HashRouter to resolve 404 Not Found on Render

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
     <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
-  </BrowserRouter>
+  </HashRouter>
 )
